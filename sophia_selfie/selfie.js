2bff8e77ebeabf9d04e85a27fde978da3a4daba3
Sophia L <slinarez423@gmail.com>
2023-10-26 15:49:34 -0700

    add selfie

---
+++ sophia_selfie/selfie.js
@@ -0,0 +1,31 @@
+function setup() {  
+ createCanvas(600,600);
+}
+
+
+function draw() {
+background(220, 204, 200); // Dark blue color
+fill(235,190,150);//skin
+ellipse(300,600,330,400);//bod
+ellipse(300,275,300,333);//head
+fill(255, 255, 255); //eye - white
+ellipse(235,255,50,30);//left eye
+ fill(10,10,10);//pupil color
+ellipse(230,255,25,30);//left pupil
+fill(255, 255, 255); //eye - white
+ellipse(335,255,50,30);//right eye
+fill(10,10,10);//pupil color
+ellipse(330,255,25,30);//right pupil
+fill(255, 171, 171); //light pink
+ellipse(280,345,90,50);//mooouuth
+fill(255, 71, 71); //light pink
+arc(280, 367, 50, 50, PI, PI+PI);//:p
+
+
+
+
+
+
+
+
+}
